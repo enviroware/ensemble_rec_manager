bed95ceba4f2886b78c082fe0bbafe9f1dbaa810
enviroware <info@enviroware.com>
2021-03-27 18:06:34 +0100

    first commit

---
+++ Library.pm
@@ -0,0 +1,370 @@
+package Library;
+
+# Author: R.Bianconi - Enviroware srl
+
+use strict;
+require Exporter;
+
+our @ISA = qw(Exporter);
+our @EXPORT = qw(
+    anytrim
+    check_input
+    extract_values_from_grid
+    init_input
+    load_sq_json_file
+    load_receptors
+    node
+    set_info
+
+);
+our @EXPORT_OK = qw();
+my $VERSION = '20210306';
+
+use Data::Dumper;
+use Tie::File;
+use FileHandle;
+use JSON;
+use Date::Calc qw (Delta_DHMS Add_Delta_DHMS Delta_YMDHMS Add_Delta_YMDHMS);
+use File::Path;
+use POSIX qw(floor);
+use FindBin qw ($Bin);
+
+#-----------------------------------------------------------------------------#
+
+sub load_sq_json_file {
+
+    my %args = @_;
+
+    my $sq_json_file = $args{File};
+    my %sq_json = %{read_json_file(File=>$sq_json_file)};   
+    $sq_json{y_max} = $sq_json{y_min} + $sq_json{ny}*$sq_json{dy};
+    return %sq_json;
+
+}
+
+#-----------------------------------------------------------------------------#
+
+sub load_receptors {
+
+    my %args = @_;
+
+    my $file = $args{File};
+
+    tie my @array, 'Tie::File', $file, mode => O_RDONLY;
+    my @headers = anytrim (split(/\,/, $array[0]));
+    my %index;
+    @index{@headers} = (0..$#headers);
+
+    my $idx_lcode = $index{LCODE};
+    my $idx_lon = $index{LON};
+    my $idx_lat = $index{LAT};
+    my $idx_dutc = $index{DUTC};
+
+    my %receptors;
+
+    foreach my $ii (1..$#array) {
+
+        my @parts = anytrim (split(/\,/, $array[$ii]));
+        $receptors{$parts[$idx_lcode]}{lon} = $parts[$idx_lon];
+        $receptors{$parts[$idx_lcode]}{lat} = $parts[$idx_lat];
+        $receptors{$parts[$idx_lcode]}{dutc} = $parts[$idx_dutc];
+
+    }
+
+    return %receptors;
+    
+}
+
+#-----------------------------------------------------------------------------#
+
+sub anytrim {
+
+    my @array;
+    for ( @_ ) {
+        if ( ref ) {
+            push @array,
+                ref eq 'ARRAY'  ? [ anytrim( @$_ ) ] :
+                ref eq 'HASH'   ? { anytrim( %$_ ) } :
+                ref eq 'SCALAR' ?  \anytrim( $$_ ) :
+                ();
+                }
+        else {
+            if (defined($_)) {
+                ( my $var = $_ ) =~ s/^\s+//;
+                $var =~ s/\s+$//;
+                push @array, $var;
+                }
+            }
+        }
+    return wantarray ? @array : $array[ 0 ];
+}
+
+#--------------------------------------------------------------------#
+
+sub check_input {
+    
+    my %args = @_;
+    my $hinfo = $args{Info} || {};
+    my $hinput = $args{Input} || {};
+
+    my %info = %{$hinfo};
+    my %input = %{$hinput};
+
+    _print_info(Input=>$hinput);
+
+    my $jinp = $input{jsonfile};
+    if ($input{jsonfile}) {
+        die whoami()." The specified JSON input file ($input{jsonfile}) does not exist." unless (-e $input{jsonfile});
+        %input = %{read_json_file(File=>$input{jsonfile})};
+    } else {
+        die "Please provide a JSON file (-jsonfile=/path/to/input.json).\n";
+    }
+    $input{jsonfile} = $jinp; 
+
+    my $ret = {
+        info => $hinfo,
+        input => \%input
+    };
+
+    return $ret;
+
+}
+
+#------------------------------------------------------------------------------#
+
+sub extract_values_from_grid {
+
+    my %args = @_;
+
+    my $dat_file = $args{DatFile};
+    my @xs = @{$args{Xs}};
+    my @ys = @{$args{Ys}};
+    my @ts = @{$args{Ts}};
+    my %domain = %{$args{Domain}};
+
+    my $datasize = 14;  # ENSEMBLE AQ (V5)
+    my $bt = 14 * $domain{nx} + 1;
+    open FHH, "<$dat_file" or die "$dat_file: $!";
+
+    my @values;
+    foreach my $t (@ts) {
+        foreach my $y (@ys) {
+            foreach my $x (@xs) {
+                my $start = _variable_node_index(
+                    X=>$x,Y=>$y,T=>$t,
+                    DxDomain=>$domain{dx},
+                    DyDomain=>$domain{dy},
+                    NxDomain=>$domain{nx},
+                    NyDomain=>$domain{ny},
+                    BytesLine=>$bt,
+                    XminDomain=>$domain{x_min},
+                    YmaxDomain=>$domain{y_max},
+                    IsAirQuality=>1,
+                );
+                my $value;
+                seek(FHH,$start,0);
+                read(FHH,$value,$datasize);
+                push @values, $value*1.;
+            }
+        }
+    }
+    close FHH;
+
+    return @values;
+
+}
+
+#------------------------------------------------------------------------------#
+
+sub init_input {
+
+    my @numerics = qw();
+
+    my @flags = qw(
+        jsonfile
+    );
+
+    my @input_list;
+    my %input;
+
+    map {
+        push @input_list, "$_=i" => \$input{$_};
+    } (@numerics);
+    map {
+        push @input_list, "$_=s" => \$input{$_};
+    } (@flags);
+
+    my $ret = {
+        input_list => \@input_list,
+        input => \%input
+    };
+
+    return $ret;
+
+}
+
+#--------------------------------------------------------------------#
+
+sub _load_manual {
+
+    my $manual = {
+        jsonfile  => qq(The path and name of the JSON input file),
+    };
+
+    return $manual;
+
+}
+
+#--------------------------------------------------------------------#
+
+sub _print_info {
+
+    my %args = @_;
+    my $hinput = $args{Input} || die whoami();
+    my $show_help_info = $args{ShowHelpInfo};
+
+    my %input = %{$hinput};
+
+    my $manual = _load_manual();
+
+    if ($show_help_info) {
+    print "\n
+    Type perl $0.pl -info=ITEM to get help.\n
+    The help is available for the following ITEMS:
+        * jsonfile   : the path and name of the JSON file with input
+        \n";
+        exit;
+
+    };
+
+    return unless ($input{info});
+
+    my $item = $input{info} || '';
+
+    return unless ($item);
+
+    my $content = $manual->{$item} || '';
+ 
+    print "** $item **\n";
+    if ($content) {
+        print $manual->{$item},"\n";
+    } else {
+        print "No manual entry.\n";
+    }
+     
+    exit;
+
+}
+
+#--------------------------------------------------------------------#
+
+sub read_json_file {
+
+    my %args = @_;
+    my $file = $args{File};
+
+    local $/;
+    my $fh = FileHandle->new;
+    open ($fh,"<$file") or die whoami()."$file: $!";
+    my $json_text = <$fh>;
+    close ($fh);
+    my $json = decode_json( $json_text );
+
+    return $json;
+
+}
+
+#-------------------------------------------------------------------
+
+sub node {
+
+    # This subroutine calculates the closest grid node coordinate
+
+    my $r_name = 'GeneralPurpose.pm (node)';
+    $r_name .= "***\n".join("\n",caller())."\n*** Error:";
+
+    my %args = @_;
+
+
+    die $r_name." Value argument is mandatory." unless (exists($args{Value}));
+    my $grid_size = $args{GridSize} or die $r_name." GridSize argument is mandatory.";
+
+    my $val = $args{Value};
+
+    my $grid = $val / $grid_size;
+    my $int_grid = POSIX::floor($grid);
+
+    my $diff;
+    $diff = $grid - $int_grid;
+
+    my $node;
+    if  ($diff < 0.5) {
+        $node = $int_grid * $grid_size;
+    } else {
+        $node = ($int_grid + 1) * $grid_size;
+    }
+
+    return $node;
+}
+
+#--------------------------------------------------------------------#
+
+sub set_info {
+
+    my %args = @_;
+
+    my $hinfo = $args{Info} || die whoami();
+    my $hinput = $args{Input} || die whoami();
+    my $logger = $args{Logger} || die whoami();
+
+    my %info = %{$hinfo};
+    my %input = %{$hinput};
+ 
+    my $ret = {
+        info => \%info,
+        input => $hinput
+    };
+
+    return $ret;
+
+}
+
+#--------------------------------------------------------------------#
+
+sub _variable_node_index {
+
+    my %args = @_;
+
+    my $pos;
+    my $starting_byte;
+
+    #    ENSEMBLE AIR QUALITY
+
+    # Data in files are stored as matrices, preceded by the date and time record
+    # The position in the file of value(x,y,t) is:
+    #my $nx = ($xmax - $xmin) / $dx + 1;
+    #my $pos = (($ymax - $y) / $dy) * $nx + (($x - $xmin) / $dx) + t * $ny * (14*$nx +1) * ;
+
+    my $nmat = $args{BytesLine} * $args{NyDomain};
+    my $nbefore = ($nmat + 13) * $args{T};
+
+    $pos = POSIX::floor ( 0.5 +
+                   POSIX::floor( 0.5 + (($args{YmaxDomain} - $args{Y}) / $args{DyDomain}) ) * ( $args{NxDomain} * 14 + 1)
+                 + POSIX::floor( 0.5 + (($args{X} - $args{XminDomain}) / $args{DxDomain}) * 14 )
+              );
+    die $pos, Dumper \%args if ($pos < 0);
+
+    #The starting byte is:
+
+    $starting_byte = $nbefore + 13 + $pos;
+
+    return $starting_byte;
+
+}
+
+#--------------------------------------------------------------------#
+
+sub whoami { ( caller(1) )[3]." - " }
+
+1;
+
---
+++ rec_extractor_lst.pl
@@ -0,0 +1,189 @@
+use strict;
+
+# Author: rbianconi@enviroware.com
+
+my $VERSION = '20210321';
+
+if ($^O eq 'MSWin32') {
+    print "This code must be run under Linux or macOS\n";
+    exit();
+}
+use FindBin;
+use lib $FindBin::Bin;
+
+use Library qw(
+    init_input
+    check_input
+    extract_values_from_grid
+    set_info
+    load_sq_json_file
+    load_receptors
+    node
+);
+use Getopt::Long;
+use Data::Dumper;
+use File::Basename;
+use File::Path;
+use IO::Uncompress::Bunzip2 qw(bunzip2 $Bunzip2Error) ;
+use Log::Log4perl qw(:easy);
+use POSIX qw (strftime floor);
+use IPC::Run qw( run );
+use Date::Calc qw( Add_Delta_DHMS );
+
+Log::Log4perl->easy_init($INFO);
+my $logger = Log::Log4perl->get_logger();
+
+# Read user input from JSON file and load defaults
+
+my $show_help_info = '';
+my $ret = init_input();
+my @input_list = @{$ret->{input_list}};
+my $hinput = $ret->{input};
+
+GetOptions(@input_list,'help'=>\$show_help_info);
+
+my $ret = check_input(Input=>$hinput,Logger=>$logger);
+my $hinfo = $ret->{info};
+$hinput = $ret->{input};
+
+$ret = set_info(Info=>$hinfo,Input=>$hinput,Logger=>$logger);
+$hinfo = $ret->{info};
+
+my %input = %{$hinput};
+my %info = %{$hinfo};
+
+my $sq = $input{sq};
+my $cs = $input{cs};
+my $rl = $input{rl};
+my $vr = $input{vr};
+
+# Load sequence info
+my %sq_json = load_sq_json_file(File=>"$input{home_dir}{json}/$sq.json");
+my $date = $sq_json{cases}{"c$cs"}{first_output};
+my @start_date = unpack("a4a2a2a2a2",$sq_json{cases}{"c$cs"}{start});
+my $missing = $sq_json{cases}{"c$cs"}{releases}{"r$rl"}{variables}{"v$vr"}{missing_value};
+my $src_file = "$input{home_dir}{src}/$sq-$cs.src";
+
+# Loop on models
+my @models = @{$input{models}};
+foreach my $mo (@models) {
+
+    my $base = "$mo-$sq-$cs-$rl-$vr-$date";
+    my $vbase = "v$vr-$mo-$sq-$cs-$rl-$date";
+    my $bz2_file = "$input{home_dir}{bz2}/$mo/$sq/$cs/$base.ens.bz2";
+    my $ens_file = "$input{home_dir}{ens}/s$sq/c$cs/$base.ens";
+    my $dat_file = "$input{home_dir}{dat}/s$sq/c$cs/r$rl/v$vr/$vbase.dat";
+    my $out_folder = "$input{home_dir}{out}/$sq/$cs/$rl/$vr/$mo";
+    my $dat_log_file = "$input{home_dir}{dat}/s$sq/c$cs/$base.log";
+
+    # Uncompress ens.bz2 grid file (optional)
+    if ($input{go}{bunzip2}) {
+        die "$bz2_file: $!" unless (-e $bz2_file);
+        my ($basename,$path,$suffix) = fileparse($ens_file,'.ens');
+        if ($input{create_dirs}) {
+            mkpath $path unless (-e $path);
+        }
+        my $status = bunzip2 $bz2_file => $ens_file or die "bunzip2 failed: $Bunzip2Error\n";
+    }
+
+    # Decode ens grid file (optional)
+    if ($input{go}{deform}) {
+        #  1,INPUT_PATH
+        #  2,INPUT_FILE
+        #  3,SOURCE_FILE
+        #  4,MODEL_NAME
+        #  5,UPL_DATE
+        #  6,DELTA_M
+        #  7,DELTA_U
+        #  8,UPL_USER
+        #  9,UPL_IP
+        # 10,OUTPUT_BASE_PATH
+        my $now = strftime("%Y%m%d%H%M", localtime(time));
+
+        my $status_c_folder = "$input{home_dir}{dat}/s$sq/c$cs/status_c";
+        mkpath $status_c_folder unless (-e $status_c_folder);
+        my $status_r_folder = "$input{home_dir}{dat}/s$sq/c$cs/r$rl/status_r";
+        mkpath $status_r_folder unless (-e $status_r_folder);
+        for my $iv (1..$input{vrmax}) {
+            my $vro =  sprintf "%2.2d", $iv;
+            my $dat_folder = "$input{home_dir}{dat}/s$sq/c$cs/r$rl/v$vro";
+            mkpath $dat_folder unless (-e $dat_folder);
+        }
+        my @command = (
+            $input{executables}{deform},
+            "$input{home_dir}{ens}/s$sq/c$cs/",
+            $base.'.ens',
+            $src_file,'DUMMY',$now,'+0m','+0m','dummy','127.0.0.1',
+            "$input{home_dir}{dat}/"
+        );
+
+        my $cmd = join (" ",@command);
+        run "$cmd > $dat_log_file 2>&1";
+
+    }
+
+    # Extract UTC from receptors
+    if ($input{go}{extract}) {
+
+        mkpath $out_folder unless (-e $out_folder);
+
+        my $precision = $sq_json{cases}{"c$cs"}{releases}{"r$rl"}{variables}{"v$vr"}{precision};
+
+        my $decimals = - log($precision) / log(10);
+        # Generate output_dates
+#       my @datetimes = generate_datetimes(Start=$sq_json{first_output_utc});
+
+        # Load pool of receptors
+        my %receptors = load_receptors(File=>$input{pool_file});
+
+        my @ts = (1..$sq_json{nt});
+
+        # Loop on receptors
+        foreach my $lcode (sort keys %receptors) {
+            my @xs = (node(Value=>$receptors{$lcode}{lon},GridSize=>$sq_json{dx}));
+            my @ys = (node(Value=>$receptors{$lcode}{lat},GridSize=>$sq_json{dy}));
+            my $dutc = $receptors{$lcode}{dutc};
+            die "DUTC not integer ($dutc) for $lcode" if (floor($dutc) < $dutc);
+
+            my $fdate_lst = sprintf "%.4d-%.2d-%.2d".'T'."%.2d:%.2d:00", @start_date[0..4];
+
+            my @values = extract_values_from_grid(
+                            DatFile =>$dat_file,
+                            Xs => \@xs,
+                            Ys => \@ys,
+                            Ts => \@ts,
+                            Domain => \%sq_json);
+            my $out_file = "$out_folder/$lcode-$mo-$sq-$cs-$rl-$vr.csv";
+            open(OUT,">$out_file") or die "$out_file:$!";
+            foreach my $it_lst (@ts) {
+
+                # Apply time shift to time series
+
+                my @t_date_lst = Add_Delta_DHMS(@start_date,0,
+                                              0,0,$it_lst*$sq_json{dt_out},0);
+                my $tdate_lst = sprintf "%.4d-%.2d-%.2d".'T'."%.2d:%.2d:00", @t_date_lst[0..4];
+
+                my $valp = $missing;
+                my $idx_utc = $it_lst - $dutc + 1;
+                if ($idx_utc < 0) {
+                } else {
+                    $valp = sprintf "%.${decimals}f", $values[$idx_utc];
+                }               
+                print OUT "$valp,$fdate_lst,$tdate_lst\n";
+                print "$valp,$fdate_lst,$tdate_lst\n";
+
+                $fdate_lst = $tdate_lst;
+            }
+
+            # Save time series 
+            close (OUT);
+
+        }
+
+
+# Apply optional time averaging and save time series
+
+# Delete dat grid file (optional)
+    }
+}
+
